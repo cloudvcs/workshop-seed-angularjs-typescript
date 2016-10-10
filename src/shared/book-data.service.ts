09665b3568149a5c5d5326159ef6bc2cf9072c2e
Elmar Burke <elmar.burke@symetics.com>
2016-10-10 10:53:28 +0200

    fix service http->$http

--- src/shared/book-data.service.ts
+++ src/shared/book-data.service.ts
@@ -3,7 +3,9 @@ import * as angular from 'angular';
 export const bookDataName = 'bookData';
 
 export class BookData {
-  constructor(private http: angular.IHttpService) {}
+  constructor(
+    private $http: angular.IHttpService
+  ) {}
 
   getBooks() {
     return [{name: 'Momo'}, {name: 'Harry Potter'}];
