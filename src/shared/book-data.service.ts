0e59ad741c83a207bb760590d251adf7cbf43cf1
Elmar Burke <elmar.burke@symetics.com>
2016-10-10 10:29:19 +0200

    service prep http

--- src/shared/book-data.service.ts
+++ src/shared/book-data.service.ts
@@ -3,7 +3,7 @@ import * as angular from 'angular';
 export const bookDataName = 'bookData';
 
 export class BookData {
-  constructor() {}
+  constructor(private http: angular.IHttpService) {}
 
   getBooks() {
     return [{name: 'Momo'}, {name: 'Harry Potter'}];
